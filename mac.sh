4891fdc89a3b6f0f079a809ea6f6a9b63c187e56
jihwanK <jhwan1228@gmail.com>
2020-03-12 21:19:02 +0900

    update

---
+++ mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# jihwan's dev_env for mac
+
+# install home brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install iTerms
+brew install iterms
+
+# install 
---
+++ ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# jihwan's dev_env for ubuntu
+sudo apt install -y vim htop
